7ed9a22419e00a50575905321c41b0db89e49997
Will <wmfitt@Hotmail.com>
2019-04-10 22:57:01 -0600

    notes for next steps

--- main.js
+++ main.js
@@ -6,26 +6,26 @@ let teeName;
 loadDoc();
 
 function loadDoc() {
-   let xhttp = new XMLHttpRequest();
-   $('.modal').modal({backdrop: 'static', keyboard: false});
-   $('.modal').modal('show');
-   xhttp.onreadystatechange = function () {
-       if (this.readyState == 4 && this.status == 200) {
-        let courseList = JSON.parse(this.responseText);
-           console.log(courseList);
-           for(let i = 0; i < courseList.courses.length; i++) {
-               $("#modal-container").append(
-                   `<div class="card text-center course-select" style="width: 18rem;">  
+    let xhttp = new XMLHttpRequest();
+    $('.modal').modal({ backdrop: 'static', keyboard: false });
+    $('.modal').modal('show');
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let courseList = JSON.parse(this.responseText);
+            console.log(courseList);
+            for (let i = 0; i < courseList.courses.length; i++) {
+                $("#modal-container").append(
+                    `<div class="card text-center course-select" style="width: 18rem;">  
                     <h5 class="card-title">${courseList.courses[i].name}</h5>
                     <img src="${courseList.courses[i].image}" class="card-img-top" alt="Golf Course Picture">
                     <div class="card-body">
                     <button class="btn btn-success" data-dismiss="modal" onclick="getCourseById(${courseList.courses[i].id})">Select</button>
                     </div></div>`);
-           } 
-       }
-   };
-   xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
-   xhttp.send();
+            }
+        }
+    };
+    xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
+    xhttp.send();
 }
 
 function getCourseById(id) {
@@ -48,7 +48,7 @@ function getCourseById(id) {
 
 function loadTeeById() {
     tees = courseData.holes[0].teeBoxes;
-    for(let i = 0; i < tees.length && i < 4; i++) {
+    for (let i = 0; i < tees.length && i < 4; i++) {
         $("#modal-title").html(`${courseData.name}, Tee Select`)
         $("#modal-container").append(
             `<div class="card text-center course-select">  
@@ -56,7 +56,7 @@ function loadTeeById() {
              <h5 class="card-title">${tees[i].teeType}</h5>
              <button class="btn btn-success text-center" id="${tees.indexOf(tees[i])}" onclick="choosePlayerCount(${tees.indexOf(tees[i])})">Select</button>
              </div></div>`);
-    } 
+    }
 
 }
 
@@ -65,7 +65,7 @@ function choosePlayerCount(id) {
     teeName = tees[id].teeType;
     $("#modal-container").empty();
     playerCount = [1, 2, 3, 4];
-    for(let i = 0; i < playerCount.length; i++) {
+    for (let i = 0; i < playerCount.length; i++) {
         $("#modal-title").html(`${courseData.name}, ${tees[id].teeType}'s Tee, Player Amount Select`)
         $("#modal-container").append(
             `<div class="card text-center course-select">  
@@ -86,6 +86,3 @@ function buildTable(players) {
     buildPlayer();
     buildTotal();
 }
-
-
-
--- model.js
+++ model.js
@@ -1,17 +1,16 @@
 function buildTitle() {
     let teeNameCap = teeName.charAt(0).toUpperCase() + teeName.slice(1);
-
     $("#title-bar").append(`<span>${courseData.name} - ${teeNameCap}'s Tee</span>`)
 }
 
 function buildHoles() {
     $("#holes").append(`<div class="title border-top border-dark">Hole</div>`);
-    for(let i = 0; i < courseData.holes.length; i++) {
+    for (let i = 0; i < courseData.holes.length; i++) {
         $("#holes").append(`<div class="col-sm bg-info border border-dark" id="col_${courseData.holes[i].hole}">${i + 1}</div>`)
-        if(i === 8){
+        if (i === 8) {
             $("#holes").append(`<div class="col-sm bg-secondary text-white">Out</div>`)
         }
-        if(i === 17){
+        if (i === 17) {
             $("#holes").append(`<div class="col-sm border-right border-white bg-secondary text-white">In</div>`)
         }
     }
@@ -23,21 +22,21 @@ function buildPar() {
     let parOutTotal = 0;
     let parInTotal = 0;
     $("#par").append(`<div class="title">Par</div>`);
-    for(let i = 0; i < courseData.holes.length; i++) {
+    for (let i = 0; i < courseData.holes.length; i++) {
         let parValue = courseData.holes[i].teeBoxes[teeIndex].par;
         parTotal += parValue;
         $("#par").append(`<div class="col-sm bgColor border border-dark" id="par_${i}">${parValue}</div>`);
 
-        if(i < 9) {
+        if (i < 9) {
             parOutTotal += parValue;
         }
-        else{
+        else {
             parInTotal += parValue;
         }
-        if(i === 8){        
+        if (i === 8) {
             $("#par").append(`<div class="col-sm border-top border-white bg-secondary text-white">${parOutTotal}</div>`)
         }
-        if(i === 17){
+        if (i === 17) {
             $("#par").append(`<div class="col-sm border-top border-right border-white bg-secondary text-white">${parInTotal}</div>`)
         }
     }
@@ -49,21 +48,21 @@ function buildYardage() {
     let yardageOutTotal = 0;
     let yardageInTotal = 0;
     $("#yardage").append(`<div class="title">Yardage</div>`);
-    for(let i = 0; i < courseData.holes.length; i++) {
+    for (let i = 0; i < courseData.holes.length; i++) {
         let yardageValue = courseData.holes[i].teeBoxes[teeIndex].yards;
         yardageTotal += yardageValue;
         $("#yardage").append(`<div class="col-sm bgColor border border-dark" id="yardage_${i}">${yardageValue}</div>`);
 
-        if(i < 9) {
+        if (i < 9) {
             yardageOutTotal += yardageValue;
         }
-        else{
+        else {
             yardageInTotal += yardageValue;
         }
-        if(i === 8){
+        if (i === 8) {
             $("#yardage").append(`<div class="col-sm border-top border-white bg-secondary text-white">${yardageOutTotal}</div>`)
         }
-        if(i === 17){
+        if (i === 17) {
             $("#yardage").append(`<div class="col-sm border-top border-right border-white bg-secondary text-white">${yardageInTotal}</div>`)
         }
     }
@@ -75,21 +74,21 @@ function buildHandicap() {
     let handicapOutTotal = 0;
     let handicapInTotal = 0;
     $("#handicap").append(`<div class="title">Handicap</div>`);
-    for(let i = 0; i < courseData.holes.length; i++) {
+    for (let i = 0; i < courseData.holes.length; i++) {
         let handicapValue = courseData.holes[i].teeBoxes[teeIndex].hcp;
         handicapTotal += handicapValue;
         $("#handicap").append(`<div class="col-sm bgColor border border-dark" id="handicap_${i}">${handicapValue}</div>`);
 
-        if(i < 9) {
+        if (i < 9) {
             handicapOutTotal += handicapValue;
         }
-        else{
+        else {
             handicapInTotal += handicapValue;
         }
-        if(i === 8){
+        if (i === 8) {
             $("#handicap").append(`<div class="col-sm border-top border-white bg-secondary text-white">${handicapOutTotal}</div>`)
         }
-        if(i === 17){
+        if (i === 17) {
             $("#handicap").append(`<div class="col-sm border-top border-right border-white bg-secondary text-white">${handicapInTotal}</div>`)
         }
     }
@@ -98,7 +97,7 @@ function buildHandicap() {
 
 
 function buildPlayer() {
-    for(let i = 0; i < playerCount; i++){
+    for (let i = 0; i < playerCount; i++) {
         //fix player input, change to span or div?
         $("#players").append(`<div id="player${i}" class="player-container"><div class="title" data-text="Player ${i + 1}..." contenteditable="true" onkeyup="addPlayer(this.value, event)"></div></div>`);
         buildScore(i);
@@ -107,12 +106,12 @@ function buildPlayer() {
 
 function buildScore(index) {
     let playerIdDiv = "#player" + index;
-    for(let i = 0; i < courseData.holes.length; i++) {
+    for (let i = 0; i < courseData.holes.length; i++) {
         $(playerIdDiv).append(`<input type="number" tabindex="${i + 2}" onkeyup="calculateTotal(this)" class="col-sm border border-dark">`);
-        if(i === 8){
+        if (i === 8) {
             $(playerIdDiv).append(`<div class="col-sm out border-top border-white bg-secondary text-white"></div>`)
         }
-        if(i === 17){
+        if (i === 17) {
             $(playerIdDiv).append(`<div class="col-sm in border-top border-right border-white bg-secondary text-white"></div>`)
         }
     }
@@ -133,42 +132,44 @@ function calculateTotal(event) {
     let total = 0;
     let outValue = 0;
     let inValue = 0;
-    
-    $(inputId).each(function(index, testVal) {
+
+    $(inputId).each(function (index, testVal) {
         let value = Number($(this).val());
-        
+
         total += value;
-        if(index <= 8){
+        if (index <= 8) {
             let currentOutValue = Number($(this).val());
             outValue += currentOutValue;
             $(outId).text(outValue);
         }
-      });
-      $(totalId).text(total);
-      inValue = total - outValue;
-      $(inId).text(inValue);
+    });
+    $(totalId).text(total);
+    inValue = total - outValue;
+    $(inId).text(inValue);
 }
 
 //NEEDS WORK
-function addPlayer(event) {
+// function addPlayer(event) {
 
-}
+// }
 
-class PlayerGenerator {
-    constructor() {
-        this.players = [];
-    }
+// class PlayerGenerator {
+//     constructor() {
+//         this.players = [];
+//     }
 
-    add(id, name) {
-        this.players.push(new Player(id, name, scoreArray));
-    }   
+//     add(id, name) {
+//         this.players.push(new Player(id, name, scoreArray));
+//     }
 
-}
+// }
 
-class Player {
-    constructor(id, name, scoreArray) {
-        this.scoreArray = [];
-        this.name = name;
-        this. id = id;
-    }
-}
+// class Player {
+//     constructor(id, name, scoreArray) {
+//         this.scoreArray = [];
+//         this.name = name;
+//         this.id = id;
+//     }
+// }
+
+//on focus for adding player, use set for name management 
